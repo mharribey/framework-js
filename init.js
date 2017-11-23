c06a06a1cb68543d86b7144778933eeec6fed100
mharribey <mathieuharribey@hotmail.fr>
2017-11-23 15:49:56 +0100

    to php

--- init.js
+++ init.js
@@ -2,6 +2,6 @@ var lp = document.querySelector("#launchpad");
 
 for(var i = 1;i<6;i++){
   for(var j = 1;j<6;j++){
-    lp.innerHTML += "<div class='item' id='"+i+j+"' style='grid-column:"+i+"/"+i+";grid-row:"+j+"/"+j+"'>papa</div>"
+    lp.innerHTML += "<div class='item' id='"+i+j+"' style='grid-column:"+i+"/"+i+";grid-row:"+j+"/"+j+"'></div>"
   }
 }
