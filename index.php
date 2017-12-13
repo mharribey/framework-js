09976501bcce454580bd6940a8ad837d348a1cc2
mharribey <mathieuharribey@hotmail.fr>
2017-12-13 16:53:33 +0100

    reset previous

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ ini_set('display_errors', 1);
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
     <title>test</title>
     <link rel="stylesheet" href="style.css">
   </head>
